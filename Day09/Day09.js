c76bf27d6eec778bc0d1bb818ee2ee5d8b401e0c
bleakwulf <bleakwulf@gmail.com>
2022-12-11 16:49:01 +0000

    cleanup of D09 solution

--- Day09/Day09.js
+++ Day09/Day09.js
@@ -1,20 +1,20 @@
 const fs = require('fs')
 
-const DEFAULT_MAX_ELASTICITY_UNIT = 2
-const DEFAULT_MIN_KNOTS = 2
-const DEFAULT_MAX_KNOTS = 10
+const DEFAULT_MAX_ELASTICITY_UNIT  = 2
+const DEFAULT_MIN_KNOTS  = 2
+const DEFAULT_MAX_KNOTS  = 10
 
 const directionMultipliers = new Map([
-  [`U`, [0, 1]],
-  [`D`, [0, -1]],
-  [`L`, [-1, 0]],
-  [`R`, [1, 0]]
+  [ `U`, [  0,  1 ] ],
+  [ `D`, [  0, -1 ] ],
+  [ `L`, [ -1,  0 ] ],
+  [ `R`, [  1,  0 ] ]
 ])
 
 let rawInputData, inputData
 
 try {
-  rawInputData = fs.readFileSync(`${__dirname}/demo2.txt`, 'utf8')
+  rawInputData = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
 } catch (e) {
   console.log(`Error!`)
   console.error(e)
@@ -30,19 +30,19 @@ const parseHeadMoves = () => {
     })
 }
 
-const performMoves = (knotCount = DEFAULT_MIN_KNOTS) => {
-  let knots = Array.from({ length: knotCount }, () => [0, 0])
+const performMoves = ( knotCount = DEFAULT_MIN_KNOTS ) => {
+  let knots = Array.from( { length: knotCount }, () => [ 0, 0 ] )
 
   let coveredPoints = new Set()
   coveredPoints.add(`0,0`)  // add starting point
 
-  inputData.forEach(refMove => {
-    let [xMove, yMove] = refMove
-    let increment = (xMove || yMove) < 0 ? -1 : +(!!xMove || !!yMove)
+  inputData.forEach( refMove => {
+    let [ xMove, yMove ] = refMove
+    let increment = ( xMove || yMove ) < 0 ? -1 : +( !!xMove || !!yMove )
     let iteration = 0
 
-    while (iteration !== (xMove || yMove)) {
-      let [[xHeadCoord, yHeadCoord], ...body] = knots
+    while ( iteration !== (xMove || yMove) ) {
+      let [ [ xHeadCoord, yHeadCoord ], ...body ] = knots
       let newBody = []
 
       xHeadCoord += !!xMove ? increment : 0
@@ -50,17 +50,16 @@ const performMoves = (knotCount = DEFAULT_MIN_KNOTS) => {
 
       iteration += increment
 
-      body.forEach((bodyUnit, bodyIndex) => {
-        let [lastXCoord, lastYCoord] = !!bodyIndex ? newBody.at(bodyIndex - 1) : [xHeadCoord, yHeadCoord]
+      body.forEach( (bodyUnit, bodyIndex) => {
+        let [lastXCoord = xHeadCoord, lastYCoord = yHeadCoord ] = newBody.at( bodyIndex - 1 ) ?? [] 
 
-        let knotGaps = bodyUnit
-          .map(
+        let knotGaps = bodyUnit.map(
             (unitCoord, coordIndex) => (coordIndex === 0 ? lastXCoord : lastYCoord) - unitCoord
           )
 
-        if (knotGaps.some(gap => Math.abs(gap) === DEFAULT_MAX_ELASTICITY_UNIT)) {
-          let [unitCoordX, unitCoordY] = bodyUnit
-          let [xGap, yGap] = knotGaps
+        if ( knotGaps.some( gap => Math.abs(gap) === DEFAULT_MAX_ELASTICITY_UNIT ) ) {
+          let [ unitCoordX, unitCoordY ] = bodyUnit
+          let [ xGap, yGap ] = knotGaps
 
           bodyUnit = [
             !xGap ? unitCoordX : unitCoordX + xGap / Math.abs(xGap),
@@ -74,7 +73,7 @@ const performMoves = (knotCount = DEFAULT_MIN_KNOTS) => {
         newBody.push(bodyUnit)
       })
 
-      knots = [[xHeadCoord, yHeadCoord], ...newBody]
+      knots = [ [ xHeadCoord, yHeadCoord ], ...newBody]
     }
   })
 
