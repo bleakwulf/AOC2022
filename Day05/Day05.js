26fcee54df96418da04745db01abc62f028ec5fd
bleakwulf <bleakwulf@gmail.com>
2022-12-27 09:25:53 +0000

    - cleanup of D05 solution

--- Day05/Day05.js
+++ Day05/Day05.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
 let CRANE_VERSIONS = {
-  CRATE_MOVER_9000: { isMultiCargo: false }, 
-  CRATE_MOVER_9001: { isMultiCargo: true }
+  CRATE_MOVER_9000  : {  isMultiCargo: false   }, 
+  CRATE_MOVER_9001  : {  isMultiCargo: true    }
 }
 
 let rawInputData, stackRawState, craneRawMoves
@@ -10,23 +10,22 @@ let stacks = new Map()
 let craneMoves = []
 
 try {
-  rawInputData = fs.readFileSync(`${__dirname}/input.txt`, 'utf8')
+  rawInputData = fs.readFileSync(`${__dirname}/Day05/input.txt`, 'utf8')
 } catch (e) {
   console.log(`Error!`)
   console.error(e)
 }
 
-const initCraneMoves = () => {
-  craneMoves = craneRawMoves
-    .replace(/move /g, '')
-    .replace(/ from /g, ',')
-    .replace(/ to /g, ',')
-    .split('\n')
-    .map(craneMove => craneMove.split(','))
-    .map(moveData => {
-      let [crateCount, ...otherData] = moveData
-      return [ +crateCount, ...otherData ]
-    })
+const parseInput = () => {
+  [ stackRawState, craneRawMoves ] = rawInputData.split('\n\n')
+  
+  craneMoves = Array.from( 
+    craneRawMoves.matchAll( /^move (?<count>(.{1,})) from (?<source>(.{1,})) to (?<target>(.{1}))$/gm ), 
+    ( match ) => {
+      let { count, source, target } = match.groups
+      return Array.of( +count, source, target )
+    }
+  )
 }
 
 const initStacksData = () => {
@@ -54,7 +53,7 @@ const initStacksData = () => {
   })
 }
 
-const doCraneMoves = (craneVersion = CRANE_VERSIONS.CRATE_MOVER_9000) => {
+const doCraneMoves = ( craneVersion = CRANE_VERSIONS.CRATE_MOVER_9000 ) => {
   craneMoves.forEach( craneMove => {
     let [ crateCount, stackOrigin, stackDestination ] = craneMove
     let cargo = stacks.get(stackOrigin).splice(0, crateCount)
@@ -63,31 +62,27 @@ const doCraneMoves = (craneVersion = CRANE_VERSIONS.CRATE_MOVER_9000) => {
   })
 }
 
-const getTopCrates = () => Array.from(stacks.values())
-  .reduce( (result, currentStack) => result + currentStack.at(0) || '', '')
-
-const solveP1 = () => {
+const getTopCrates = (craneVersion = CRANE_VERSIONS.CRATE_MOVER_9000) => {
   initStacksData()
-  doCraneMoves()
-  return getTopCrates()
+  doCraneMoves(craneVersion)
+  
+  return Array
+    .from(    stacks.values() )
+    .reduce(  (result, currentStack) => result + currentStack.at(0) || '', '' )
 }
 
-const solveP2 = () => {
-  initStacksData()
-  doCraneMoves(CRANE_VERSIONS.CRATE_MOVER_9001)
-  return getTopCrates()
-}
+const solveP1 = () => getTopCrates()
+
+const solveP2 = () => getTopCrates(CRANE_VERSIONS.CRATE_MOVER_9001)
 
 console.log("AOC2022 | Day 05")
 console.time("AOC2022 D05")
 
 if (rawInputData) {
-  [stackRawState, craneRawMoves] = rawInputData.split('\n\n')
-  
-  initCraneMoves()
+  parseInput()
 
-  console.log(`P1 : ${solveP1()}`)
-  console.log(`P2 : ${solveP2()}`)
+  console.log(`P1 : ${ solveP1() }`)
+  console.log(`P2 : ${ solveP2() }`)
 }
 
 console.timeEnd("AOC2022 D05")
