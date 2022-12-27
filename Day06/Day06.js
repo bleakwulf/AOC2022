404142cb548977f40c98860de862e68dc0a52e9d
bleakwulf <bleakwulf@gmail.com>
2022-12-27 09:04:07 +0000

    - cleanup of D06 solution

--- Day06/Day06.js
+++ Day06/Day06.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
 const MARKER_TYPES = {
-  START_OF_PACKET: { length: 4 },
-  START_OF_MESSAGE: { length: 14 }
+  START_OF_PACKET   : { length  :  4 },
+  START_OF_MESSAGE  : { length  : 14 }
 }
 
 let rawInputData
@@ -15,29 +15,25 @@ try {
 }
 
 const getMaxIndexForMarker = (markerType = MARKER_TYPES.START_OF_PACKET) => {
-  for (let i = 0; i < rawInputData.length; i++) {
+  for ( let i = 0; i < rawInputData.length; i++ ) {
     let maxIndex = i + markerType.length
-    if (new Set(rawInputData.slice(i, maxIndex)).size === markerType.length)
+    if ( new Set( rawInputData.slice( i, maxIndex) ).size === markerType.length )
       return maxIndex
   }
 
   return 0
 }
 
-const solveP1 = () => {
-  return getMaxIndexForMarker()
-}
+const solveP1 = () => getMaxIndexForMarker()
 
-const solveP2 = () => {
-  return getMaxIndexForMarker(MARKER_TYPES.START_OF_MESSAGE)
-}
+const solveP2 = () => getMaxIndexForMarker(MARKER_TYPES.START_OF_MESSAGE)
 
 console.log("AOC2022 | Day 06")
 console.time("AOC2022 D06")
 
 if (rawInputData) {
-  console.log(`P1 : ${solveP1()}`)
-  console.log(`P2 : ${solveP2()}`)
+  console.log(`P1 : ${ solveP1() }`)
+  console.log(`P2 : ${ solveP2() }`)
 }
 
 console.timeEnd("AOC2022 D06")
